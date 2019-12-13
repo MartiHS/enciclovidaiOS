c8fd6ef8e143b1a8cba96c086e7ba44c20a5af1c
Jose Marti <marty_135@hotmail.com>
2019-12-12 23:48:54 -0600

    Eliminación de archivos no compatibles

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
-import React, {Component} from 'react';
-import AppNavigator from './src/Navigator/AppNavigation';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
-export default class App extends Component{
+export default class HelloWorldApp extends Component {
   render() {
-    return <AppNavigator/>;
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Hello, world!</Text>
+      </View>
+    );
   }
 }
--- src/Theme/Metrics.js
+++
@@ -1,9 +0,0 @@
-const metrics = {
-    navBarHeight: 56,
-    buttonSize: 50,
-    tabBarHeight: 46,
-    smallSpace: 8,
-    baseSpace: 16,
-}
-
-export default metrics;
