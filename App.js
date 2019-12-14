b82aa20e262ad8895cc8deef300cbdc5ef123e73
Jose Marti <marty_135@hotmail.com>
2019-12-13 23:23:59 -0600

    Funcionamiento de la navegación

--- App.js
+++ App.js
@@ -1,53 +1,11 @@
 
-
-
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Button,
-  View,
-  SafeAreaView,
-  Text,
-  Alert,
-} from 'react-native';
+import AppNavigator from './src/Navigator/AppNavigation';
 
-class HomeScreen extends Component {
-  static navigationOptions = {
-    title: 'Welcome',
-  };
+export default class App extends Component{
   render() {
-    const {navigate} = this.props.navigation;
-    return (
-      <Button
-        title="Go to Jane's profile"
-        onPress={() => navigate('Profile', {name: 'Jane'})}
-      />
-    );
+    return <AppNavigator/>;
   }
 }
 
-class ProfileScreen extends Component {
-  static navigationOptions = {
-    title: 'Welcome',
-  };
-  render() {
-    const {navigate} = this.props.navigation;
-    return (
-      <Button
-        title="Go to Jane's profile"
-        onPress={() => navigate('Profile', {name: 'Jane'})}
-      />
-    );
-  }
-}
-
-const MainNavigator = createStackNavigator({
-  Home: {screen: HomeScreen},
-  Profile: {screen: ProfileScreen},
-});
-
-const App = createAppContainer(MainNavigator);
-
-export default App;
+/*  - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
