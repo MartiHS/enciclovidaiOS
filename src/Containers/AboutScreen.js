fc4a516683e5f195afea9050b710837034162ccc
MartiHS <Marti@MacBook-de-Marti.local>
2020-03-04 22:10:02 -0600

    Se gregó la búsqueda por BDI cuando no hay nada en naturalista

--- src/Containers/AboutScreen.js
+++ src/Containers/AboutScreen.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-
 import { View, Text, Image, Alert, ScrollView, BackHandler } from 'react-native';
 import { withNavigation } from "react-navigation";
 import { createIconSetFromFontello } from "react-native-vector-icons";
-
 import Spinner from 'react-native-loading-spinner-overlay';
+
 import NavBar from '../Components/NavBar';
 import TabBar from "../Components/TabBar";
 import config from "../Theme/Fonts/config"
-import styles from "../Components/Styles/AboutScreenStyles";
-
+import styles from "../Components/Styles/AboutScreenStyles"; 
+ 
 const CustomIcon = createIconSetFromFontello(config);
 
 const SPECIE_PHOTOS_ENDPOINT = 'http://enciclovida.mx/especies/';
@@ -29,7 +28,7 @@ class AboutScreen extends Component {
     this.UNSAFE_componentWillReceiveProps = this.UNSAFE_componentWillReceiveProps.bind(this);
   }
 
-  getSpecieImageUrl(id_specie) {
+  getSpecieImageUrl(id_specie) {   
     fetch(`${SPECIE_PHOTOS_ENDPOINT}${id_specie}/fotos-naturalista.json`)
       .then(res => res.json())
       .then((json) => {
@@ -37,6 +36,24 @@ class AboutScreen extends Component {
         this.setState({ imagen: specieImage });
         console.log(`${SPECIE_PHOTOS_ENDPOINT}${id_specie}/fotos-naturalista.json`);
         console.log(json);
+      }).catch(error => {
+        // En el caso de que el servicio fotos-naturalista no devuelva fotos, llamara BDI
+        this.getSpecieImageFromBDI(id_specie);
+        console.log(error);
+      });
+  }
+ 
+  getSpecieImageFromBDI(id_specie) {
+    fetch(`${SPECIE_PHOTOS_ENDPOINT}${id_specie}/fotos-bdi.json`)
+      .then(res => res.json())
+      .then((json) => {
+        console.log("BDI");
+        console.log(json.fotos);
+        console.log(json.fotos[0].medium_url);
+        let specieImage = json.fotos && json.fotos.length > 0 ? json.fotos[0].medium_url : "";
+        this.setState({ imagen: specieImage });
+        console.log(`${SPECIE_PHOTOS_ENDPOINT}${id_specie}/fotos-bdi.json`);
+        console.log("json");
       }).catch(error => {
         Alert.alert("Error en los datos");
         console.log(error);
@@ -84,6 +101,8 @@ class AboutScreen extends Component {
               else
                 return data;
             });
+          } else {
+            result = ["No existe ninguna descripción para esta especie"];
           }
         }
         catch (e) {
@@ -97,11 +116,12 @@ class AboutScreen extends Component {
       });
   }
 
-  /**to prevent reload data from the same specie */
+  /* to prevent reload data from the same specie */
   setAboutIdSpecie = (id_specie) => {
     global.about_id_specie = id_specie;
   }
 
+  // Función para llamar a servicios de enciclovida: Llamar sólo cuando la especie en curso cambia.
   async fetchData(id_specie, about_id_specie) {
     if (id_specie != about_id_specie) {
       this.setState({ imagen: "", contenido_render_array: [], spinner: true });
@@ -118,33 +138,17 @@ class AboutScreen extends Component {
   }
 
 
-
+  // se invoca antes de que un componente montado reciba nuevos props
   UNSAFE_componentWillReceiveProps(props) {
-    console.log("\n\n - - UNSAFE_componentWillReceiveProps - - \n\n");
+    console.log("\n\n - - UNSAFE_componentWillReceiveProps desde AboutScreen- - \n\n");
     //Alert.alert("idProps", this.state.load.toString());
     this.fetchData(global.id_specie, global.about_id_specie);
   }
-
-  // Se invoca inmediatamente después de que la actualización ocurra. Este método no es llamado para el renderizador inicial
-  componentDidUpdate(props) {
-    console.log("\n\n - - componentDidUpdate - - \n\n");
-  }
-
-  // Para avisar a React si la salida de un componente no se ve afectada por el cambio actual en el estado o los accesorios
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log("\n\n - - shouldComponentUpdate - - \n\n");
-    return false; 
-  }
-
-  // se invoca justo antes de llamar al método de render
-  static getDerivedStateFromProps(props, state) {
-    console.log("\n\n - - getDerivedStateFromProps - - \n\n");
-    console.log("\n\n\nLlamada a AboutScreen \n" + JSON.stringify(props.navigation.state.params.id_specie));
-  }
-
   // Se invoca inmediatamente después de que un componente se monte (se inserte en el árbol)
   componentDidMount() {
     console.log("\n\n - - componentDidMount - - \n\n");
+    console.log("id_specie: " + global.id_specie + "  -  about_id_specie: " + global.about_id_specie);
+
     this.fetchData(global.id_specie, global.about_id_specie);
     /*this.backHandler = BackHandler.addEventListener('hardwareBackPress', ()=>{
         console.log("back_about");
@@ -156,10 +160,28 @@ class AboutScreen extends Component {
     // this.s3! = this.props.navigation.addListener('didBlur', this.onEvent);
   }
 
+    /*/ Se invoca inmediatamente después de que la actualización ocurra. Este método no es llamado para el renderizador inicial
+  componentDidUpdate(props) {
+    console.log("\n\n - - componentDidUpdate - - \n\n");
+    return true;
+  }*/
+
+  /*/ Para avisar a React si la salida de un componente no se ve afectada por el cambio actual en el estado o los accesorios
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("\n\n - - shouldComponentUpdate - - \n\n");
+    return true; 
+  }*/
+
+  /*/ se invoca justo antes de llamar al método de render
+  static getDerivedStateFromProps(props, state) { 
+    console.log("\n\n - - getDerivedStateFromProps - - \n\n");
+    console.log("Llamada a AboutScreen \n" + JSON.stringify(props.navigation.state.params.id_specie));
+    return true; 
+  }*/
   
   // Se invoca inmediatamente antes de desmontar y destruir un componente
   componentWillUnmount() {
-    console.log("\n\n - - componentWillUnmount - - \n\n");
+    //console.log("\n\n - - componentWillUnmount - - \n\n");
     // this.s0!.remove();
     // this.s1!.remove();
     // this.s2!.remove();
