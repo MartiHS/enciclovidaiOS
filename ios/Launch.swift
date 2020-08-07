ccd59f6eca04bcd3a826880623c6b4c6dd89c4fe
MartiHS <Marti@MacBook-de-Marti.local>
2020-08-06 20:35:43 -0500

    Nueva forma de mostrar resumen de especie

---
+++ ios/Launch.swift
@@ -0,0 +1,30 @@
+//
+//  Launch.swift
+//  Enciclovida
+//
+//  Created by Marti Hernandez on 02/04/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import UIKit
+
+class Launch: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
